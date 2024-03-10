d4667853586ae53e6ed5ba0e7d0b60d41ef4832a
Marine C <mariine.becker@gmail.com>
2024-03-10 22:13:21 +0100

    fonctions tournoi ok

--- src/tournamentGenerator.js
+++ src/tournamentGenerator.js
@@ -1,51 +1,73 @@
 class TournamentGenerator {
-  constructor(teams) {
-      this.teams = teams; // Chaque équipe est un objet { name: string, players: array }
-      this.poules = [];
-      this.finalStages = []; // Les phases finales
-  }
+    // CONSTRUCTEUR
+    constructor(teams) {
+        this.teams = teams; // Chaque équipe est un objet { name: string, players: array }
+        this.poules = [];
+        this.finalStages = []; // Les phases finales
+    }
 
+    //   Générer les poules
+    generatePoules() {
+        let shuffledTeams = [...this.teams].sort(() => 0.5 - Math.random());
+        const nbPoules = Math.floor(shuffledTeams.length / 4);
+        for (let i = 0; i < nbPoules; i++) {
+            this.poules.push(shuffledTeams.splice(0, 4));
+        }
+        console.log("Poules générées :", this.poules);
+    }
 
-  generatePoules() {
-      let shuffledTeams = [...this.teams].sort(() => 0.5 - Math.random());
-      const nbPoules = Math.floor(shuffledTeams.length / 4);
-      for (let i = 0; i < nbPoules; i++) {
-          this.poules.push(shuffledTeams.splice(0, 4));
-      }
-      console.log("Poules générées :", this.poules);
-  }
+    // Obtenir les poules
+    getPoules() {
+        return this.poules;
+    }
 
+    // Simuler les matchs de poules
+    simulatePoulesMatches() {
+        let qualifiedTeams = [];
+        this.poules.forEach(poule => {
+            qualifiedTeams.push(...poule.slice(0, 2));
+        });
+        this.finalStages.push(qualifiedTeams);
+        console.log("Équipes qualifiées pour les phases finales :", this.finalStages[0]);
+    }
 
-  simulatePoulesMatches() {
-      let qualifiedTeams = [];
-      this.poules.forEach(poule => {
-          qualifiedTeams.push(...poule.slice(0, 2));
-      });
-      this.finalStages.push(qualifiedTeams);
-      console.log("Équipes qualifiées pour les phases finales :", this.finalStages[0]);
-  }
+    // Génèrer les phases finales
+    generateFinalStages() {
+        let currentStage = this.finalStages[0];
+        while (currentStage.length > 1) {
+            let nextStage = [];
+            for (let i = 0; i < currentStage.length; i += 2) {
+                let winner = currentStage[i + (Math.random() > 0.5 ? 0 : 1)];
+                nextStage.push(winner);
+            }
+            this.finalStages.push(nextStage);
+            currentStage = nextStage;
+        }
+        console.log("Phases finales :", this.finalStages);
+    }
 
+    // Obtenir les phases finales
+    getFinalStages() {
+        return this.finalStages;
+    }
 
-  generateFinalStages() {
-      let currentStage = this.finalStages[0];
-      while (currentStage.length > 1) {
-          let nextStage = [];
-          for (let i = 0; i < currentStage.length; i += 2) {
-              let winner = currentStage[i + (Math.random() > 0.5 ? 0 : 1)];
-              nextStage.push(winner);
-          }
-          this.finalStages.push(nextStage);
-          currentStage = nextStage;
-      }
-      console.log("Phases finales :", this.finalStages);
-  }
+    // Générer le tournoi final
+    generateTournament() {
+        this.generatePoules();
+        this.simulatePoulesMatches();
+        this.generateFinalStages();
+        return this.finalStages;
+    }
 
-  generateTournament() {
-      this.generatePoules();
-      this.simulatePoulesMatches();
-      this.generateFinalStages();
-      return this.finalStages;
-  }
+    // Déterminer le gagnant des phases finales
+    determineWinner() {
+        return this.finalStages[this.finalStages.length - 1][0];
+    }
+
+    // Obtenir les teams
+    getTeams() {
+        return this.teams;
+    }
 }
 
-export default TournamentGenerator;
+module.exports = TournamentGenerator;
