84e2b2a6230739bd0c4121669c72181b5a4071b3
Marine C <mariine.becker@gmail.com>
2024-03-10 20:01:59 +0100

    ajout fonctions teamGenerator

--- src/teamGenerator.js
+++ src/teamGenerator.js
@@ -1,10 +1,13 @@
 class TeamGenerator {
+  
+  // CONSTRUCTEUR
   constructor(players, playersPerTeam = 3) {
     this.players = players;
     this.playersPerTeam = playersPerTeam;
     this.teams = [];
   }
 
+  // Générer une team aléatoire
   generateTeams() {
     let shuffledPlayers = [...this.players].sort(() => 0.5 - Math.random()); // Mélange aléatoire des joueurs
     let teamIndex = 0;
@@ -21,11 +24,43 @@ class TeamGenerator {
     }
   }
 
+  // Obtenir la team
   getTeams() {
     return this.teams;
   }
+
+  // Réinitialiser la team
+  resetTeams() {
+    this.teams = [];
+  }
+
+  // Obtenir les joueurs 
+  getPlayers() {
+    return this.players;
+  }
+
+  // Ajouter des joueurs
+  addPlayers(newPlayers) {
+    this.players = [...this.players, ...newPlayers];
+  }
+
+  // Supprimer des joueurs
+  removePlayers(playersToRemove) {
+    this.players = this.players.filter(player => !playersToRemove.includes(player));
+  }
+
+  // Obtenir le nombre de joueurs total
+  playersNumber(num) {
+    this.playersNumber = num;
+  }
+
+  // Obtenir le nombre de teams
+  totalTeams() {
+    return this.teams.length;
+  }
+
 }
 
 export default  TeamGenerator
-// Exemple d'utilisation
+
 
